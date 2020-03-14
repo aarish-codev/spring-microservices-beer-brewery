19f5af5c06cdd35fe83848c495ad31e93b7dd169
AarishS <aarish.work@gmail.com>
2020-03-14 20:33:04 +0530

    project setup

---
+++ src/test/java/spring/beer/brewery/BeerBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.beer.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
